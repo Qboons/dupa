812ae7ecce554f7199d578795acc0f1983fd522b
qboons <jakkoz91@gmail.com>
2018-05-29 10:11:13 +0200

    style changes

--- src/pages/Layout/Nav/SearchBar.js
+++ src/pages/Layout/Nav/SearchBar.js
@@ -12,7 +12,6 @@ import {  getQuery } from '../../../actions/index';
 
     onInputChange(term) {
         this.setState({term});
-        
     }
     handleClick(){
         if(this.myInputRef.value == ""){
@@ -23,17 +22,17 @@ import {  getQuery } from '../../../actions/index';
         this.resetForm();
         console.log(this.myInputRef.value);
     }
+ 
     resetForm = () => { 
         this.myFormRef.reset();
       }
-
 
     render() {
         return (
             <div className="col-sm-3 col-md-3">
             <form className="navbar-form" role="search" ref={(el) => this.myFormRef = el}>
             <div className="input-group search">
-                <input  type="text" className="form-control" ref={(el) => this.myInputRef = el} placeholder="Search for..." onChange={event => this.onInputChange(event.target.value)}/>
+                <input type="text" className="form-control" ref={(el) => this.myInputRef = el} placeholder="Search for..." onChange={event => this.onInputChange(event.target.value)}/>
                 <div className="input-group-btn">
                 <Link to="advertisements" query={{ query: this.state.term }}>
                     <button className="btn btn-default" type="submit" onClick={this.handleClick} ><i className="glyphicon glyphicon-search"></i></button>
